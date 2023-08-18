da3c718fc8f1f52cb591eb9c245c1ba320f3ec22
Gkalina Borisevits <gkalinaborisevits@gmail.com>
2023-08-18 18:19:36 +0200

    add: task

--- Task_61.java
+++ Task_61.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Task_61 {
 
  // Найти минимальное чётное число в последовательности положительных чисел или вывести -1,
@@ -5,6 +7,20 @@ public class Task_61 {
  // Оценить временную и пространственную сложность алгоритма.
 
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int min = Integer.MAX_VALUE;
+    System.out.println("Введите необходимое количество чисел");
+    int count = scanner.nextInt();
+    System.out.println("Введите " + count + " положительных чисел, каждое с новой строки");
+
+    for (int i = 0; i < count; i++) {
+      int number = scanner.nextInt();
+
+      if(number % 2 == 0 && number < min){
+        min = number;
+      }
+    } System.out.println(min);
 
   }
 }
